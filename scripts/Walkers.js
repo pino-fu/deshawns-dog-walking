edc29f8ae4992c9817264dd4e88de5b7acc8f0f8
Ben Hoberg <benjhoberg@gmail.com>
2022-12-08 15:36:06 -0600

    Added new utility for walkers to work in several cities rather than just one. Clicking on a given walker now results in a display of all cities that the walker works in

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,62 +1,58 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getWalkerCities, getCities } from "./database.js"
+
+const walkers = getWalkers()
+const cities = getCities()
+const walkerCities = getWalkerCities()
 
 
-document.addEventListener(
-    "click",  // This is the type of event
-    (clickEvent) => {
-        /*
-            The target of a click event is the most specific HTML element
-            that was clicked by the user.
-        */
-        const itemClicked = clickEvent.target
+const walkerCitiesperWalker = (walker) => {
+    let assignmentArr = []
 
-        /*
-            Only run the rest of the logic if a walker <li> was clicked
-        */
-        if (itemClicked.id.startsWith("walker")) {
-
-            /*
-                Extract the primary key from the id attribute of the list
-                item that you clicked on. Refer back to the code you
-                wrote for each list item. Note the format of the id
-                attribute ("walker--2" if you clicked on the second one).
-
-                This code splits that string apart into an array, and
-                captures the "2" and assigns it to be the value of the
-                `walkerId` variable.
-
-                Splitting a string in JavaScript:
-                    https://www.youtube.com/watch?v=u2ZocmM93yU
-
-                Destructuring in JavaScript:
-                    https://www.youtube.com/watch?v=UgEaJBz3bjY
-            */
-            const [, walkerId] = itemClicked.id.split("--")
-
-            /*
-                Now that you have the primary key of a walker object,
-                find the whole object by iterating the walkers array.
-            */
-            for (const walker of walkers) {
+    for (const walkerTown of walkerCities) {
+        if (walkerTown.walkerId === walker.id) {
+            assignmentArr.push(walkerTown)
+        }
+    }
+    return assignmentArr
+}
 
 
-                /*
-                    Compare the primary key of each walker to the one
-                    you have. As soon as you find the right one, display
-                    the window alert message.
-                */
-                if (walker.id === parseInt(walkerId)) {
+const CitiesWhereYouWork = (assignArr) => {
+    let citiesMatched = ""
 
-                    window.alert(`${walker.name} services ${walker.city}`)
-                }
+    for (const assigned of assignArr) {
+        for (const town of cities) {
+            if (town.id === assigned.cityId) {
+                citiesMatched = `${citiesMatched} ${town.name}`
             }
         }
     }
-)
+    return citiesMatched
+}
 
 
+document.addEventListener(
+    "click",  
+    (clickEvent) => {
+   
+        const itemClicked = clickEvent.target
 
-const walkers = getWalkers()
+        if (itemClicked.id.startsWith("walker")) {
+
+            const [, walkerId] = itemClicked.id.split("--")
+
+            for (const walker of walkers) {
+
+                if (walker.id === parseInt(walkerId)) {
+                    const assignments = walkerCitiesperWalker(walker)
+                    const cities = CitiesWhereYouWork(assignments)
+
+                    window.alert(`${walker.name} services ${cities}`)
+                }    
+            }    
+        }    
+    }    
+)    
 
 
 export const Walkers = () => {
